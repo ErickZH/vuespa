0dd7d9a9f9864ae35a04bb01ca9f287f93061597
Erick Zarate <zaraterick@outlook.com>
2018-04-19 17:01:04 -0500

    Initial commit

---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Laravel</div>
+
+                <div class="card-body">
+                    Header
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/assets/js/store.js
@@ -0,0 +1,12 @@
+export default {
+	state: {
+		welcomeMessage: 'Bienvenido'	
+	},
+	mutations: {},
+	getters: {
+		welcome(state) {
+			return state.welcomeMessage;
+		}
+	},
+	actions: {}
+};
